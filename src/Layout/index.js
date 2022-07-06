5f1dcd016ff65dd158c80042e9666557356ce0be
Jon Eastin <picardncce@gmail.com>
2022-07-06 08:53:55 -0700

    added deck view and routing

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -3,19 +3,26 @@ import { Switch, Route } from 'react-router-dom'
 
 import Header from './Header'
 import Home from './Home'
+import Study from './Study'
+import Deck from './Deck'
 
 function Layout() {
 	return (
 		<div className='container'>
-			<Header /> {/*  */}
+			<Header />
 			<Switch>
 				<Route exact path='/'>
 					<Home /> {/* listDecks() route = '/' */}
 				</Route>
+				<Route exact path='/decks/:deckId/study'>
+					<Study /> {/* route = '/decks/:deckId/study' */}
+				</Route>
+				<Route exact path='/decks/:deckId'>
+					<Deck /> {/* readDeck() route = '/decks/:deckId' */}
+				</Route>
 			</Switch>
-			{/* route = '/decks/:deckId/study' */}
 			{/* route = '/decks/new' */}
-			{/* readDeck() route = '/decks/:deckId' */}
+
 			{/* updateDeck() route = '/decks/:deckId/edit' */}
 			{/* route = '/decks/:deckId/cards/new' */}
 			{/* route = '/decks/:deckId/cards/:cardId/edit' */}
