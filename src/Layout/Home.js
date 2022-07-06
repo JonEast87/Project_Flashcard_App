ed445b2cbcd221020cd0382211065116d7b588ce
Jon Eastin <picardncce@gmail.com>
2022-07-06 07:54:51 -0700

    setup the necessary pages and got the home route working

---
+++ src/Layout/Home.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+
+import { deleteDeck, listDecks } from '../utils/api/index'
+import { Button } from './Button'
+
+function Home() {
+	const [deckList, setDeckList] = useState([])
+	const history = useHistory([])
+
+	useEffect(() => {
+		async function loadDecks() {
+			try {
+				const response = await listDecks()
+				setDeckList(response)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		loadDecks()
+	}, [])
+
+	if (deckList) {
+		return (
+			<div>
+				{deckList.map((deck) => (
+					<div key={deck.id} className='card mb-3'>
+						<div className='card-body'>
+							<h4 className='card-title'>{deck.name}</h4>
+							<h6 className='card-subtitle mb-2 text-muted'>
+								{deck.cards.length}
+							</h6>
+							<p className='card-text'>{deck.description}</p>
+							<Link to={`/decks/${deck.id}`}>
+								<Button>View</Button>
+							</Link>
+							<button
+								className='btn btn-danger'
+								onClick={() => {
+									if (window.confirm('Delete this deck?')) {
+										deleteDeck(`${deck.id}`)
+										history.go('/')
+									}
+								}}>
+								Delete
+							</button>
+						</div>
+					</div>
+				))}
+			</div>
+		)
+	} else {
+		return <p>Loading...</p>
+	}
+}
+
+export default Home
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
+import Header from './Header'
+import Home from './Home'
+
 function Layout() {
-	return <div className='container'></div>
+	return (
+		<div className='container'>
+			<Header /> {/*  */}
+			<Switch>
+				<Route exact path='/'>
+					<Home /> {/* listDecks() route = '/' */}
+				</Route>
+			</Switch>
+			{/* route = '/decks/:deckId/study' */}
+			{/* route = '/decks/new' */}
+			{/* readDeck() route = '/decks/:deckId' */}
+			{/* updateDeck() route = '/decks/:deckId/edit' */}
+			{/* route = '/decks/:deckId/cards/new' */}
+			{/* route = '/decks/:deckId/cards/:cardId/edit' */}
+			{/* route = NotFound */}
+		</div>
+	)
 }
 
 export default Layout
